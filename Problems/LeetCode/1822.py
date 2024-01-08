7237096d8f0522210afe0bb485deaa7bdedc973c
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-07 22:34:05 -0600

    1822

---
+++ Problems/LeetCode/1822.py
@@ -0,0 +1,5 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        if 0 in nums: return 0
+        negcount = len([x for x in nums if x < 0])
+        return 1 if negcount%2==0 else -1
