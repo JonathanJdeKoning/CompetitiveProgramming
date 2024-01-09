f1526c416b8959de5c78d450ae7f25b7d0bb664a
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-08 22:56:22 -0600

    babelfish

---
+++ Problems/Kattis/babelfish.py
@@ -0,0 +1,47 @@
+import os
+import sys 
+from math import ceil, floor, pi
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
+
+##################################################################
+DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
+debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
+if DEBUG: os.system('color')                                     #
+##################################################################
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+##################################################################
+prt = sys.stdout.write                                           #
+def debug(var, name=""):                                         #
+    if DEBUG:                                                    #
+        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
+def out(x):                                                      #
+    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
+if DEBUG: print = out                                            #
+def intspls():                                                   #
+    ints = list(map(int, sys.stdin.readline().strip().split()))  #
+    return ints if len(ints)>1 else ints[0]                      #
+def intpls(): return int(input())                                #
+def listpls(): return input().split()                            #
+def stringpls(): return sys.stdin.readline().strip()             #
+##################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def solve():
+    words = {}
+    
+    while True:
+        entry = input().split()
+        if not entry: break
+        words[entry[1]] = entry[0]
+    while True:
+        try:
+            word = input()
+            print(words.get(word, "eh"))
+        except EOFError:
+            break
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################
+if __name__ == "__main__": #
+    solve()                #
+############################
