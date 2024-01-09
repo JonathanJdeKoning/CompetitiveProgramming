5126a890278756a181951f4f3732df2fd0e5af4e
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-09 01:31:23 -0600

    greedily increasing

---
+++ Problems/Kattis/greedilyincreasing.py
@@ -0,0 +1,46 @@
+import os
+import sys 
+from math import ceil, floor, pi
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
+
+##################################################################
+DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
+debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
+if DEBUG: os.system('color')                                     #
+##################################################################
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+##################################################################
+prt = sys.stdout.write                                           #
+def debug(var, name=""):                                         #
+    if DEBUG:                                                    #
+        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
+def out(x):                                                      #
+    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
+if DEBUG: print = out                                            #
+def intspls():                                                   #
+    ints = list(map(int, sys.stdin.readline().strip().split()))  #
+    return ints if len(ints)>1 else ints[0]                      #
+def intpls(): return int(input())                                #
+def listpls(): return input().split()                            #
+def stringpls(): return sys.stdin.readline().strip()             #
+##################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def solve():
+    numnums = int(input())
+    nums = list(map(int, input().split()))
+    ans = []
+
+    prev = 0
+    for num in nums:
+        if num > prev:
+            ans.append(num)
+            prev = num
+    print(len(ans))
+    print(" ".join([str(x) for x in ans]))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################
+if __name__ == "__main__": #
+    solve()                #
+############################
