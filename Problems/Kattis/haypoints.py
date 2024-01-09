b905acb9e2150563c51ad1e90d435756d3d7422c
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-08 23:13:07 -0600

    haypoints

---
+++ Problems/Kattis/haypoints.py
@@ -0,0 +1,52 @@
+import os
+import sys 
+from math import ceil, floor, pi
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
+
+##################################################################
+DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
+debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
+if DEBUG: os.system('color')                                     #
+##################################################################
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+##################################################################
+prt = sys.stdout.write                                           #
+def debug(var, name=""):                                         #
+    if DEBUG:                                                    #
+        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
+def out(x):                                                      #
+    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
+if DEBUG: print = out                                            #
+def intspls():                                                   #
+    ints = list(map(int, sys.stdin.readline().strip().split()))  #
+    return ints if len(ints)>1 else ints[0]                      #
+def intpls(): return int(input())                                #
+def listpls(): return input().split()                            #
+def stringpls(): return sys.stdin.readline().strip()             #
+##################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def solve():
+    numwords, numsents = intspls()
+    words = {}
+    for _ in range(numwords):
+        word, price = listpls()
+        words[word] = int(price)
+
+    for _ in range(numsents):
+        total = 0
+        while True:
+            line = listpls()
+            if line == ["."]:
+                print(total); break
+            for word in line:
+                total += words.get(word, 0)
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################
+if __name__ == "__main__": #
+    solve()                #
+############################
