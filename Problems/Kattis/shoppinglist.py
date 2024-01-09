5941ce6883a9917a8706cdcc88c74396de45e5c3
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-08 23:22:14 -0600

    shippinglist

--- Problems/Kattis/shoppinglist.py
+++ Problems/Kattis/shoppinglist.py
@@ -1,8 +1,47 @@
-lists, items = list(map(int, input().split()))
-first = input().split()
+import os
+import sys 
+from math import ceil, floor, pi
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
 
-for _ in range(lists-1):
-    new = input().split()
-    first = [x for x in first if x in new]
-print(len(first))
-print("\n".join(sorted(first)))
+##################################################################
+DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
+debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
+if DEBUG: os.system('color')                                     #
+##################################################################
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+##################################################################
+prt = sys.stdout.write                                           #
+def debug(var, name=""):                                         #
+    if DEBUG:                                                    #
+        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
+def out(x):                                                      #
+    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
+if DEBUG: print = out                                            #
+def intspls():                                                   #
+    ints = list(map(int, sys.stdin.readline().strip().split()))  #
+    return ints if len(ints)>1 else ints[0]                      #
+def intpls(): return int(input())                                #
+def listpls(): return input().split()                            #
+def stringpls(): return sys.stdin.readline().strip()             #
+##################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def solve():
+    items = defaultdict(int)
+    numlists, numitems = intspls()
+    for _ in range(numlists):
+        for item in set(listpls()):
+            items[item] += 1
+
+    good = []
+    for key, val in items.items():
+        if val == numlists:
+            good.append(key)
+    print(len(good))
+    print("\n".join(sorted(good)))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################
+if __name__ == "__main__": #
+    solve()                #
+############################
