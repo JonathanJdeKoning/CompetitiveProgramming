0934829a3db92f9608d3bfe68bd5f278b651318a
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-11 12:42:00 -0600

    codeforces chekcing

---
+++ Problems/CodeForces/A_Codeforces_Checking.py
@@ -0,0 +1,21 @@
+import os
+import sys 
+from math import ceil, floor, pi, sqrt
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
+from functools import cache
+
+
+def solve():
+    for _ in range(int(input())):
+        s =input()
+        if s in "codeforces":
+            print("YES")
+        else:
+            print("NO")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################
+if __name__ == "__main__": #
+    solve()                #
+############################
