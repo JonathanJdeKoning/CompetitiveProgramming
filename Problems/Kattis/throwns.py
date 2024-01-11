ecdb02e6cee355db51fa7dad26a1c0c0287b733f
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-11 16:08:02 -0600

    throwns

--- Problems/Kattis/throwns.py
+++ Problems/Kattis/throwns.py
@@ -30,7 +30,27 @@ def stringpls(): return sys.stdin.readline().strip()             #
 ##################################################################
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def solve():
+    n, k = intspls()
+    stack = []
+    commands = listpls()
+    prev = None
+    for command in commands:
+        debug(stack ,"stack")
+        debug(command, "command")
+        if command == "undo":
+            prev = "undo"
+            continue
 
+        if prev != "undo":
+            stack.append(command)
+            continue
+        else:
+            for i in range(int(command)):
+                stack.pop()
+                prev = None
+    total = sum([int(x) for x in stack])
+
+    print(total%n)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~
 ############################
