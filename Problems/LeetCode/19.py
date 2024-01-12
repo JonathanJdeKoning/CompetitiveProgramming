f6da8eaa6becd5ea54bb03a8d4d846c087280884
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-12 17:43:03 -0600

    19

--- Problems/LeetCode/19.py
+++ Problems/LeetCode/19.py
@@ -5,29 +5,12 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        def get_len(root):
-            mylen = 0
-            while root:
-                root = root.next
-                mylen += 1
-            return mylen
-        length = get_len(head)
-        steps = length - n
-        prev = head
-        curr = head
-
-        if n == 1 and not head.next:
-            return
-        if steps == 0:
-            return head.next
-
-        while steps != 0:
-            steps -= 1
-            prev = curr
-            curr = curr.next
-        prev.next = curr.next
-        curr.next = None
-        return head            
-        
-        
-
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        for i in range(n): right = right.next
+        while right:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return dummy.next
