2fa42fcd1ee9949596c23b8ca9c8fdb0f87ed47e
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-12 21:08:19 -0600

    200 update

--- Problems/LeetCode/200.py
+++ Problems/LeetCode/200.py
@@ -2,29 +2,25 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         seen = set()
         isles = 0
-        for i, row in enumerate(grid):
-            for j, pos in enumerate(row):
-                if pos == "1" and (i, j) not in seen:
+        rows = len(grid)
+        cols = len(grid[0])
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and (i, j) not in seen:
                     isles += 1
                     q = deque([(i, j)])
                     while q:
                         curr = q.popleft()
-                        y= curr[0]
-                        x = curr[1]
+                        y, x= curr[0], curr[1]
                         if curr in seen: continue
                         seen.add(curr)
 
-                        u = (y-1, x)
-                        d = (y+1, x)
-                        r = (y, x+1)
-                        l = (y, x-1)
+                        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+                        for dr, dc in directions:
+                            newy, newx = y+dr, x+dc
+                            if not newy in range(rows): continue
+                            if not newx in range(cols):  continue
 
-                        if u not in seen and u[0] > -1 and grid[u[0]][u[1]] == "1": 
-                            q.append(u)
-                        if d not in seen and d[0] < len(grid)and grid[d[0]][d[1]] == "1": 
-                            q.append(d)
-                        if r not in seen and r[1] < len(row) and grid[r[0]][r[1]] == "1": 
-                            q.append(r)
-                        if l not in seen and l[1] > -1 and grid[l[0]][l[1]] == "1": 
-                            q.append(l)
+                            if (newy, newx) not in seen and grid[newy][newx] == "1":
+                                q.append((newy, newx))
         return isles
