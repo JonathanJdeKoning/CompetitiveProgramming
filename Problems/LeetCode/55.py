d3d9e90df09bf5e62a9252ff9fac37aa59f953e3
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-12 20:21:36 -0600

    update 55

--- Problems/LeetCode/55.py
+++ Problems/LeetCode/55.py
@@ -1,15 +1,6 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        memo = [False]*len(nums)
-        memo[0] = True
-        clamp = len(nums)
-
-        for i, elem in enumerate(memo):
-            if elem:
-                jumps = nums[i]
-            for j in range(1,min(jumps+1, clamp-i)):
-                memo[i+j] = True
-                if i+j == len(nums)-1: return True
-        print(memo)
-        return memo[-1]
-            
+        goal = len(nums) -1
+        for i in range(len(nums)-1,-1,-1):
+            goal = i if i + nums[i] >= goal else goal
+        return goal == 0
