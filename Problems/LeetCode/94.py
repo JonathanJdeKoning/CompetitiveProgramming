abedddb337cf43b3c181cd2ebc3a324f2bf491b3
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-21 18:50:06 -0600

    94

---
+++ Problems/LeetCode/94.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        a  = []
+        def inorder(root,a):
+            if not root: return
+            inorder(root.left,a)
+            a.append(root.val)
+            inorder(root.right,a)
+            return a
+        return inorder(root, a)
