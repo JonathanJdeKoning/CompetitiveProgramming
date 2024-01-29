53c1b53c2ac66ffa1934079302728a3861c40160
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-01-29 13:44:52 -0600

    waterjournal

---
+++ Problems/Kattis/waterjournal.py
@@ -0,0 +1,54 @@
+import os
+import sys 
+from math import ceil, floor, pi, sqrt
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
+
+##################################################################
+DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
+debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
+if DEBUG: os.system('color')                                     #
+##################################################################
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+##################################################################
+prt = sys.stdout.write                                           #
+sys.setrecursionlimit(100000)                                    #
+mod = 1000000007                                                 #
+def debug(var, name=""):                                         #
+    if DEBUG:                                                    #
+        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
+def out(x):                                                      #
+    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
+if DEBUG: print = out                                            #
+def intspls():                                                   #
+    ints = list(map(int, sys.stdin.readline().strip().split()))  #
+    return ints if len(ints)>1 else ints[0]                      #
+def intpls(): return int(input())                                #
+def listpls(): return input().split()                            #
+def stringpls(): return sys.stdin.readline().strip()             #
+##################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def solve():
+    days, mn, mx = intspls()
+    entries = [intpls() for x in range(days-1)]
+    if mn not in entries and mx not in entries:
+        print(-1)
+        return
+
+    if len([x for x in entries if x > mx or x < mn]) != 0:
+        print(-1)
+        return
+    if mn not in entries:
+        print(mn)
+        return
+    if mx not in entries:
+        print(mx)
+        return
+    for i in range(mn, mx +1):
+        print(i)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################
+if __name__ == "__main__": #
+    solve()                #
+############################
