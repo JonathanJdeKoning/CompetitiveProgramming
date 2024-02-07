ab836ebb9d71c97085b430da27bd27d9583d1753
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-02-07 10:40:13 -0600

    starters120

---
+++ Contests/CodeChef/Starters120/3.py
@@ -0,0 +1,52 @@
+import os
+import sys 
+from math import ceil, floor, pi, sqrt
+from itertools import combinations, permutations
+from collections import deque, Counter, defaultdict
+
+##################################################################
+DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
+debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
+if DEBUG: os.system('color')                                     #
+##################################################################
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+##################################################################
+prt = sys.stdout.write                                           #
+sys.setrecursionlimit(100000)                                    #
+mod = 1000000007                                                 #
+def debug(var, name=""):                                         #
+    if DEBUG:                                                    #
+        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
+def out(x):                                                      #
+    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
+if DEBUG: print = out                                            #
+def intspls():                                                   #
+    ints = list(map(int, sys.stdin.readline().strip().split()))  #
+    return ints if len(ints)>1 else ints[0]                      #
+def intpls(): return int(input())                                #
+def listpls(): return input().split()                            #
+def stringpls(): return sys.stdin.readline().strip()             #
+##################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def solve():
+    for _ in range(int(input())):
+        x, y = intspls()
+        if x%y ==0: print(y)
+        elif y%x==0: print(x)
+
+        else:
+            while x != 0:
+                mn = min(x,y)
+                mx = max(x,y)
+                if mn == 0:
+                    print(mx)
+                    break
+                if mx == 0:
+                    print(mn)
+                    break
+                mod = mx%mn
+                div = mx//mn
+                y = mx-(mn*div)
+                x = mn
+if __name__ == "__main__":
+    solve()
