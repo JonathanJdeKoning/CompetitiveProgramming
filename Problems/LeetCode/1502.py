ad590e602a978d10c765371d957c1f6ec24e8508
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-03 23:12:59 -0600

    1502.py

--- Problems/LeetCode/1502.py
+++ Problems/LeetCode/1502.py
@@ -1,12 +1,8 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        arr = sorted(arr)
-        init = arr[1]-arr[0]
-        for i in range(1, len(arr)-1):
-            try:
-                if arr[i+1] - arr[i] != init:
-                    return False
-            except:
-                pass
-        return True
-
+        arr.sort()
+        diff = arr[1] - arr[0]
+        
+        for i in range(2,len(arr)):
+            if arr[i] - arr[i-1] != diff: return False
+        return True 
