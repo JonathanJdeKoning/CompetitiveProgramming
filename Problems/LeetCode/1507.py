986c43cb97a073cdee115f44f72b8731e1915bee
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-06 19:57:27 -0600

    1507

---
+++ Problems/LeetCode/1507.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        d, m, y = date.split()
+        d = d[:-2]
+        if len(d) == 1:
+            d = "0" + d
+        months = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
+
+        return (f"{y}-{months[m]}-{d}")
