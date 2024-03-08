9c5790d4fd318f23ae80e3b2bcd32fc766007da5
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-08 15:58:50 -0600

    144

--- Problems/LeetCode/144.py
+++ Problems/LeetCode/144.py
@@ -6,12 +6,7 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        @cache
         def pre(node):
             if not node: return []
-            ans = [node.val]
-            if not node.left and not node.right: return ans
-            ans+= pre(node.left)
-            ans += pre(node.right)
-            return ans
+            return [node.val] + pre(node.left) + pre(node.right)
         return pre(root)
