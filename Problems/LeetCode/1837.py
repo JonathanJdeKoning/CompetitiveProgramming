2d602625b427a9d57902358a9d4a086f56951847
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-10 15:19:01 -0500

    1837

---
+++ Problems/LeetCode/1837.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        def numberToBase(n, b):
+            if n == 0:
+                return [0]
+            digits = []
+            while n:
+                digits.append(int(n % b))
+                n //= b
+            return digits[::-1]
+        return sum(numberToBase(n,k))
