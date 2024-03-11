4322194033fbba9efca64e9ca0b1a3f848a1ea0f
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-11 12:43:43 -0500

    2287

--- Problems/LeetCode/2287.py
+++ Problems/LeetCode/2287.py
@@ -1,21 +1,6 @@
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
-        need = defaultdict(int)
-        for c in target:
-            need[c] += 1
-        
-        have = defaultdict(int)
-        for c in s:
-            if c in target:
-                have[c] += 1
-        if len(need) != len(have): return 0
-        
-        need = dict(sorted(dict(need).items()))
-        have = dict(sorted(dict(have).items()))
-        print(need)
-        print(have)
-        mx = math.inf
-        for k,v in have.items():
-            mx = min(mx, v//need[k])
-        return mx
+        need = {c:target.count(c) for c in target}
+        have = {c:s.count(c) for c in s if c in target}
+        return min([v//need[k] for k,v in have.items()]) if len(need) == len(have) else 0
             
