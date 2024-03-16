c30168b68c119334c21a35f0024de7ca0208d430
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-16 16:00:49 -0500

    1002

---
+++ Problems/LeetCode/1002.py
@@ -0,0 +1,12 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        base = list(words[0])
+        for word in words[1:]:
+            new = []
+
+            for c in word:
+                if c in base:
+                    new.append(c)
+                    base.remove(c)
+            base = new
+        return base
