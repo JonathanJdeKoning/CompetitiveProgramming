746bc0e19587bf3a3dbe20fc293c4e7669fed668
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-21 20:37:45 -0500

    530

---
+++ Problems/LeetCode/530.py
@@ -0,0 +1,22 @@
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        seen = []
+        q = deque([root])
+        while q:
+            curr = q.popleft()
+            seen.append(curr.val)
+            if curr.left: q.append(curr.left)
+            if curr.right: q.append(curr.right)
+        seen.sort()
+        mn = inf
+        for i in range(len(seen)-1):
+            dist = abs(seen[i] - seen[i+1])
+            mn = min(mn, dist)
+        return mn
