81c2b9e77826a3cc01ac6f66361eb56f622493d2
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-22 13:55:59 -0500

    653

--- Problems/LeetCode/653.py
+++ Problems/LeetCode/653.py
@@ -1,28 +1,11 @@
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        nums = []
-        q = deque([root])
+        nums = set()
+        q = [root]
         while q:
-            curr = q.popleft()
-            nums.append(curr.val)
+            curr = q.pop()
+            if k-curr.val in nums: return True
+            nums.add(curr.val)
             if curr.left: q.append(curr.left)
             if curr.right: q.append(curr.right)
-        
-
-        for num in nums:
-            other = k - num
-            if other + num != k:
-                continue
-            if other not in nums:
-                continue
-            if other == num and len([i for i in range(len(nums)) if nums[i]==num]) == 2:
-                return [i for i in range(len(nums)) if nums[i]==num]
-            elif other != num:
-                return [nums.index(num), nums.index(other)]
+        return False
