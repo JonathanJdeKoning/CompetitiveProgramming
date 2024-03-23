b7a5c8ea56a9de30fa67823896cfd4b210b2b308
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-23 17:14:05 -0500

    1185

---
+++ Problems/LeetCode/1185.py
@@ -0,0 +1,6 @@
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        days = {0:"Monday", 1:"Tuesday",2:"Wednesday",3:"Thursday",4:"Friday",5:"Saturday",6:"Sunday"}
+        from datetime import date
+        d = date(year,month,day)
+        return days[d.weekday()]
