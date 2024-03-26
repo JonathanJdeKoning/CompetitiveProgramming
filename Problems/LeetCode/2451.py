f83e6c6e8429e2cefa37ec16796fcd6458399374
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-03-26 18:09:25 -0500

    update

--- Problems/LeetCode/2451.py
+++ Problems/LeetCode/2451.py
@@ -6,17 +6,12 @@ class Solution:
             for i in range(len(word)-1):
                 diff.append(ord(word[i+1])-ord(word[i]))
             diffs.append(diff)
-        print(diffs)
-        a = diffs[0]
-        b = diffs[1]
+        a,b = diffs[0:2]
         if a==b:
             for i,diff in enumerate(diffs[2:],start=2):
                 if diff != a: 
                     return words[i]
-        else:
-            if diffs[2] != a:
-                return words[0]
-            return words[1]
+        return words[0] if diffs[2] != a else words[1]
 
 
 
