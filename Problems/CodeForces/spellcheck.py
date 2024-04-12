0e01729054309237955b9f19d857f091b642e423
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-04-11 23:51:46 -0500

    spellcheck

---
+++ Problems/CodeForces/spellcheck.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    l = int(input())
+    s= input()
+    if l!=5:
+        print("NO")
+        continue
+
+    if "T" in s and "i" in s and "m" in s and "u" in s and "r" in s:
+        print("YES")
+        continue
+    print("NO")
