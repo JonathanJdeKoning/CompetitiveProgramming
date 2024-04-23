f316d7bb0a1ac6d26d90b41fdc4959baffb20197
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-04-23 18:35:41 -0500

    proofs

--- Problems/Kattis/proofs.py
+++ Problems/Kattis/proofs.py
@@ -1,21 +1,15 @@
-lines = int(input())
-concs = []
-good = True
-for i in range(lines):
-    line = input().split()
+def solve():
+    good = set()
+    for i in range(1,int(input())+1):
+        line = input()
+        if line.startswith("->"):
+            good.add(line[3:])
+            continue
 
-    if line[0] == "->":
-        concs.append(line[-1])
-    else:
-        pivot = line.index("->")
-        asss = line[:pivot]
-        conq = line[-1]
-        for ass in asss:
-            if ass not in concs:
-                print(i+1)
-                good = False
-                break
-        concs.append(conq)
-
-if good:
-    print("correct")
+        data = line.split()
+        for axiom in data[:-2]:
+            if axiom not in good:
+                return i
+        good.add(data[-1])
+    return "correct"
+print(solve())
