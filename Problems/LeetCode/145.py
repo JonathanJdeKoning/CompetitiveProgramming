decfb2f51c49ba1aabe5bac76c8da86d65e3c086
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-04-24 13:16:49 -0500

    145

---
+++ Problems/LeetCode/145.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        out = []
+        def post(node):
+            if not node: return
+            post(node.left)
+            post(node.right)
+            out.append(node.val)
+        post(root)
+        return out
