c9f4bf66eb8fd6b7c48aa646013b2d9c3c1a9524
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-04-29 19:36:34 -0500

    74

---
+++ Problems/LeetCode/74.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            for num in row:
+                if num == target: return True
+        return False 
