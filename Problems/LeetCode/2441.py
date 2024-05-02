7de2bc034fa21a8b4ce63fa65999a2189c0735e1
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-05-01 19:37:07 -0500

    2441 update

--- Problems/LeetCode/2441.py
+++ Problems/LeetCode/2441.py
@@ -1,10 +1,3 @@
-
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-        mx = -1
-        nums = sorted(nums)
-        for num in nums:
-            if num < 0:
-                if -num in nums:
-                    mx = max(mx, -num)
-        return mx
+        return max([x for x in nums if -x in nums],default=-1)
