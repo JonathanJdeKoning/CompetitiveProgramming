0070f5034de961cdcd8fde6ddec33f361526f102
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-05-19 22:15:55 -0500

    10kindsofpeople

--- Problems/Kattis/10kindsofpeople.py
+++ Problems/Kattis/10kindsofpeople.py
@@ -1,94 +1,68 @@
-import os
-import sys 
-from math import ceil, floor, pi, sqrt
-from itertools import combinations, permutations
-from collections import deque, Counter, defaultdict
+import sys
+input = sys.stdin.readline
+class DisjointSetUnion:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.num_sets = n
 
-##################################################################
-DEBUG = os.path.isfile("C:\\Users\\jj720\\debug.txt")            #
-debugGreen,debugCyan,debugEnd = '\033[92m','\033[96m','\033[0m'  #
-if DEBUG: os.system('color')                                     #
-##################################################################
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-##################################################################
-prt = sys.stdout.write                                           #
-sys.setrecursionlimit(100000)                                    #
-mod = 1000000007                                                 #
-def debug(var, name=""):                                         #
-    if DEBUG:                                                    #
-        prt(f"{debugGreen}{name.upper()}: {var}{debugEnd}\n")    #
-def out(x):                                                      #
-    prt(f"{debugCyan}{x}{debugEnd}\n")if DEBUG else prt(f"{x}\n")#
-if DEBUG: print = out                                            #
-def intspls():                                                   #
-    ints = list(map(int, sys.stdin.readline().strip().split()))  #
-    return ints if len(ints)>1 else ints[0]                      #
-def intpls(): return int(input())                                #
-def listpls(): return input().split()                            #
-def stringpls(): return sys.stdin.readline().strip()             #
-##################################################################
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def solve():
-    rows, cols = intspls()
-    mat = []
-    for i in range(rows):
-        mat.append(list(sys.stdin.readline().strip()))
+    def find(self, a):
+        acopy = a
+        while a != self.parent[a]:
+            a = self.parent[a]
+        while acopy != a:
+            self.parent[acopy], acopy = a, self.parent[acopy]
+        return a
 
-    
-    for i in range(intpls()):
-        found = False
-        y1,x1,y2,x2 = intspls()
+    def union(self, a, b):
+        a, b = self.find(a), self.find(b)
+        if a != b:
+            if self.size[a] < self.size[b]:
+                a, b = b, a
 
-        y1 -= 1
-        y2 -= 1
-        x1 -= 1
-        x2 -= 1
-        if mat[y1][x1] != mat[y2][x2]: 
-            print("neither")
-            continue
-        person = None
-        if mat[y1][x1] == "0": person = "binary"
+            self.num_sets -= 1
+            self.parent[b] = a
+            self.size[a] += self.size[b]
+
+    def set_size(self, a):
+        return self.size[self.find(a)]
+
+    def __len__(self):
+        return self.num_sets
+
+R, C = map(int, input().split())
+mat = [list(input().strip()) for _ in range(R)]
+
+dsu = DisjointSetUnion(R*C)
+for i, row in enumerate(mat):
+    for j, cell in enumerate(row):
+        uuid = i*C+j
+
+        try:
+            next = mat[i][j+1]
+            if next == cell:
+                dsu.union(uuid, uuid+1)
+        except:pass
+
+        try:
+            down = mat[i+1][j]
+            if down == cell:
+                dsu.union(uuid,uuid+C)
+        except:pass
+q = int(input())
+for _ in range(q):
+    x1,y1,x2,y2 = map(int, input().split())
+    x1-=1
+    y1-=1
+    x2-=1
+    y2-=1
+    if dsu.find(x1*C+y1) == dsu.find(x2*C+y2):
+        if mat[x1][y1] == "0":
+            print("binary")
         else:
-            person = "decimal"
-        
-        start = mat[y1][x1]
-        end = (y2,x2)
-        
-        q = deque([(y1,x1)])
-        seen = set()
-        while q:
-            curr = q.popleft()
-            if curr in seen: continue
-            if curr == end: 
-                print(person)
-                found = True
-                break
-            seen.add(curr)
-            directions = [[1,0],[0,1],[-1,0],[0,-1]]
-
-            for dy, dx in directions:
-                y = curr[0] + dy
-                x = curr[1] + dx
-                
-                if y < 0 or x < 0 or y >= rows or x >= cols: continue
-        
-                new = (y,x)
-                if mat[new[0]][new[1]] != start:
-                    continue
-                if new not in seen:
-                    q.append(new)
-                    
-        if not found:
-            print("neither")
+            print("decimal")
+    else:
+        print("neither")
 
 
 
-
-
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~
-############################
-if __name__ == "__main__": #
-    solve()                #
-############################
