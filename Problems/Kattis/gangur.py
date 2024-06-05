12d1ab562e5d789b4e56ef74d7a4e99ab55236ad
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-06-05 12:21:53 -0500

    upgrade

--- Problems/Kattis/gangur.py
+++ Problems/Kattis/gangur.py
@@ -1,6 +1,9 @@
 s = input()
-tot = 0
-for i,c in enumerate(s):
+r = 0
+total = 0
+for c in s:
     if c == ">":
-        tot+= s[i:].count("<")
-print(tot)
+        r += 1
+    elif c == "<":
+        total += r
+print(total)
