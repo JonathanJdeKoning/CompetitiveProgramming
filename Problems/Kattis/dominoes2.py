ce824c363e6a6bcf7ff07eb3e8d93a464fbe00d0
Jonathan de Koning <61809236+JonathanJdeKoning@users.noreply.github.com>
2024-08-29 23:23:22 -0500

    dominoes2

--- Problems/Kattis/dominoes2.py
+++ Problems/Kattis/dominoes2.py
@@ -1,2 +1,28 @@
+from collections import defaultdict
 for _ in range(int(input())):
-    numTiles, numChains, numHands = map(int, input().split())
+    numNodes, numEdges, numQs = map(int, input().split())
+    edges = defaultdict(list)
+
+    for _ in range(numEdges):
+        u,v = map(int, input().split())
+        edges[u].append(v)
+
+    seen = set()
+    ans = 0
+    for _ in range(numQs):
+        q = int(input())
+
+        if q not in seen:
+            s = [q]
+            while s:
+                curr = s.pop()
+                if curr in seen: continue
+                seen.add(curr)
+                ans += 1
+
+                for edge in edges[curr]:
+                    if edge in seen: continue
+                    s.append(edge)
+    print(ans)
+
+
